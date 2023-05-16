ef21e4794f070a52f312ce570520a7fbff57ffce
EC2 Default User <ec2-user@ip-172-31-44-229.ap-northeast-1.compute.internal>
2023-05-16 09:15:48 +0000

    退会機能処理3途中

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,6 +3,26 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  before_action :customer_state, only: [:create]
+
+protected
+# 退会しているかを判断するメソッド
+def customer_state
+  ## 【処理内容1】 入力されたemailからアカウントを1件取得
+  @customer = Customer.find_by(email: params[:customer][:email])
+  ## アカウントを取得できなかった場合、このメソッドを終了する
+  return if !@customer
+  ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+  if @customer.valid_password?(params[:customer][:password])  && @customer.is_deleted == false
+    ## 【処理内容3】
+    #処理3の途中
+    redirect_to customer_session_path
+  else
+    redirect_to new_admin_session_path
+
+  end
+end
+
   # GET /resource/sign_in
   # def new
   #   super
