0c6f70e030548cc582edff29855d5dda27413550
EC2 Default User <ec2-user@ip-172-31-44-229.ap-northeast-1.compute.internal>
2023-05-16 04:42:25 +0000

    deviseモデル2作成

--- db/migrate/20230516021353_devise_create_customers.rb
+++ db/migrate/20230516021353_devise_create_customers.rb
@@ -33,6 +33,17 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
 
 
+      t.string    :family_name,         null: false
+      t.string    :first_name,          null: false
+      t.string    :family_name_kana,    null: false
+      t.string    :first_name_kana,     null: false
+      t.string    :phone_number,        null: false
+      t.string    :post_code,           null: false
+      t.text      :address,             null: false
+      t.boolean   :is_deleted,          null: false, default: false
+
+
+
       t.timestamps null: false
     end
 
