e9847131e391addc525a0a7e4ced2affcf11806f
728d <sakichichaaaaan@gmail.com>
2023-05-17 10:13:53 +0000

    5/17/19:13

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,6 +2,15 @@ class Admin::GenresController < ApplicationController
   def index
   end
 
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+  end
+
   def edit
   end
+
+  def update
+  end
+
 end
