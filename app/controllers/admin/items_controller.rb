d01251b88578964f68b2abf6e74e67c858d9d243
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-05-18 07:47:50 +0000

    createpage

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
+    @items = Item.all
   end
 
   def new
@@ -12,17 +13,25 @@ class Admin::ItemsController < ApplicationController
 
   def edit
   end
-  
+
   def create
-    item = Item.new
-    
-    if item.save
-      redirect_to admin_item_path(item.id)
-    else
-      redirect_back fallback_location: root_path
-    end
+    item = Item.new(item_params)
+
+    item.save
+    redirect_to admin_item_path(item.id)
+    # else
+    #   redirect_back fallback_location: root_path
+    # end
   end
-  
+
   def update
   end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :item_name, :item_description, :price, :is_selling, :image)
+  end
+
 end
