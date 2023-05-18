adcbdb6ad53275a766d9efc2c68c87cf091b991e
EC2 Default User <ec2-user@ip-172-31-44-229.ap-northeast-1.compute.internal>
2023-05-18 12:32:17 +0000

    顧客新規登録画面作成/登録可能

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  #before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
+  #def new
   #   super
-  end
+  #end
 
   # POST /resource
-  def create
+  #def create
+    #byebug
   #   super
-  end
+  #end
 
   # GET /resource/edit
   # def edit
@@ -38,12 +39,12 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+   def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana,:first_name_kana, :post_code, :address, :phone_number])
+   end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
