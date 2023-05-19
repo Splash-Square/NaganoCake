225e0ea6e51826d5bb20c28d7285117a0d9fad3c
keynoup <126676009+keynoup@users.noreply.github.com>
2023-05-19 08:28:21 +0000

    About完成1

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,11 +1,13 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @customer = Customer.all.page(params[:page])
+    @customers = Customer.all.page(params[:page])
   end
 
   def show
+    
   end
 
   def edit
   end
+  
 end
