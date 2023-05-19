4881bcef0e30edc8e072dc799c2abf8cc594cc17
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-05-19 07:30:27 +0000

    itemCRUD

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -13,19 +13,27 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def create
     item = Item.new(item_params)
 
-    item.save
-    redirect_to admin_item_path(item.id)
-    # else
-    #   redirect_back fallback_location: root_path
-    # end
+    if item.save
+      redirect_to admin_item_path(item.id)
+    else
+      redirect_back fallback_location: root_path
+    end
   end
 
   def update
+    item = Item.find(params[:id])
+
+    if item.update(item_params)
+      redirect_to admin_item_path(item.id)
+    else
+      redirect_back fallback_location: root_path
+    end
   end
 
 
