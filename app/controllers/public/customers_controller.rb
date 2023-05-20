d3c4e14388f4365aee9b1875afe44cee2f28a21d
EC2 Default User <ec2-user@ip-172-31-44-229.ap-northeast-1.compute.internal>
2023-05-20 02:17:20 +0000

    public会員情報確認編集画面の実装完了

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,17 +1,33 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def confirm_withdraw
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "User-introduction was successfully updated."
+      redirect_to customer_path(@customer.id)
+    else
+      render :edit
+    end
   end
 
   def withdraw
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email,:family_name, :first_name, :family_name_kana,:first_name_kana, :post_code, :address, :phone_number)
+  end
+
 
 end
