a8382788e559b8506ee291f271ee64bb2702864f
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-05-20 11:39:31 +0000

    first

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items.all
   end
 
   def update
@@ -12,5 +13,19 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    
+    if @cart_item.save
+      @cart_items = current_customer.cart_items.all
+      render 'index'
+    else
+      redirect_back fallback_location: root_path
+    end
   end
+
+
+private
+    def cart_item_params
+        params.require(:cart_item).permit(:customer_id, :item_id, :quantity)
+    end
 end
