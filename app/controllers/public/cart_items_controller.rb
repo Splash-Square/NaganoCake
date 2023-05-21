a39f78606610a124818ba1def0ff53b5aa389204
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-05-21 02:52:59 +0000

    presentwip

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -14,8 +14,15 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    
-    if @cart_item.save
+    exist_item = current_customer.cart_items.find_by(item_id: params[:item_id])
+
+    if exist_item.present?
+      exist_item.quantity += params[:quantity].to_i
+
+      exist_item.save
+      redirect_to cart_items_path
+
+    elsif @cart_item.save
       @cart_items = current_customer.cart_items.all
       render 'index'
     else
@@ -25,7 +32,9 @@ class Public::CartItemsController < ApplicationController
 
 
 private
+
+
     def cart_item_params
-        params.require(:cart_item).permit(:customer_id, :item_id, :quantity)
+        params.require(:cart_item).permit(:customer_id, :item_id, :quantity, :price)
     end
 end
