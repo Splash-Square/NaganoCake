4f7b3c6612896165ec2851be5eca359e83d2b155
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-05-24 04:36:33 +0000

    confirmview

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,14 +5,42 @@ class Public::OrdersController < ApplicationController
 
   end
 
-  #def confirm
-  #end
+  def confirm
+    @neworder = Order.new
+    @cart_items = current_customer.cart_items.all
+    @totalprice = 0
 
-  #def completed
-  #end
+    if params[:order][:select_address] == "0"
+      @neworder.post_code = current_customer.post_code
+      @neworder.address = current_customer.address
+      @neworder.name = current_customer.family_name + current_customer.first_name
 
-  #def create
-  #end
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @neworder.post_code = @address.post_code
+      @neworder.address = @address.address
+      @neworder.name = @address.name
+
+    elsif params[:order][:select_address] == "2"
+      @neworder.post_code = params[:order][:post_code]
+      @neworder.address = params[:order][:address]
+      @neworder.name = params[:order][:name]
+
+    else
+      render 'new'
+    end
+  end
+
+  def completed
+  end
+
+  def create
+    @neworder = Order.new
+    @neworder.customer_id = current_customer.id
+    @cart_items = current_customer.cart_items.all
+
+    @neworder.save
+  end
 
   #def index
   #end
@@ -23,7 +51,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:post_code,:address,:name, :payment_method,:billing_amount, :postage)
+    params.require(:order).permit(:post_code, :address, :name, :payment_method, :billing_amount, :postage)
   end
 
 end
