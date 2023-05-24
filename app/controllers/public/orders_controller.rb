374ce903b573d93db39da1501272c29e919f8f79
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-05-24 06:23:32 +0000

    ordersave

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,30 +1,32 @@
 class Public::OrdersController < ApplicationController
   def new
-    @neworder = Order.new
-    @neworder.customer_id = current_customer.id
+    @order = Order.new
+    @order.customer_id = current_customer.id
 
   end
 
   def confirm
-    @neworder = Order.new
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
     @cart_items = current_customer.cart_items.all
     @totalprice = 0
 
+    # 自身の住所が選択された場合
     if params[:order][:select_address] == "0"
-      @neworder.post_code = current_customer.post_code
-      @neworder.address = current_customer.address
-      @neworder.name = current_customer.family_name + current_customer.first_name
-
+      @order.post_code = current_customer.post_code
+      @order.address = current_customer.address
+      @order.name = current_customer.family_name + current_customer.first_name
+    # 配送先の住所が選択された場合
     elsif params[:order][:select_address] == "1"
       @address = Address.find(params[:order][:address_id])
-      @neworder.post_code = @address.post_code
-      @neworder.address = @address.address
-      @neworder.name = @address.name
-
+      @order.post_code = @address.post_code
+      @order.address = @address.address
+      @order.name = @address.name
+    # 新しい住所が選択された場合
     elsif params[:order][:select_address] == "2"
-      @neworder.post_code = params[:order][:post_code]
-      @neworder.address = params[:order][:address]
-      @neworder.name = params[:order][:name]
+      @order.post_code = params[:order][:post_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
 
     else
       render 'new'
@@ -35,11 +37,21 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    @neworder = Order.new
-    @neworder.customer_id = current_customer.id
-    @cart_items = current_customer.cart_items.all
+    # 注文データを保存
+    @order = Order.new(order_params)
+    
+    @order.save
+    redirect_to orders_conpleted_path
+    # カート内商品の情報を一つずつ注文商品に格納
+    # @order_detail = Order_detail.new
+    # @cart_items = current_customer.cart_items.all
 
-    @neworder.save
+    # @cart_items.each do |cart_item|
+    #   @order_detail.item_id = cart_item.item_id
+    #   @order_detail.order_id = @order.id
+    #   @order_detail.quantity = cart_item.quantity
+    #   @order_detail.transaction_price = @order.billing_amount
+    # end
   end
 
   #def index
@@ -51,7 +63,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:post_code, :address, :name, :payment_method, :billing_amount, :postage)
+    params.require(:order).permit(:customer_id, :post_code, :address, :name, :order_status, :payment_method, :billing_amount, :postage)
   end
 
 end
