74a49716f1beb736088c0532fab0a7cdd2c4f138
728d <sakichichaaaaan@gmail.com>
2023-05-24 06:46:12 +0000

    ジャンルコントローラー修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -8,9 +8,9 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
-    @genre = Genre.newa
-    @genre.save
-    redirect_to admin_genres_path
+  @genre = Genre.new(genre_params)
+  @genre.save
+  redirect_to admin_genres_path
   end
 
   def edit
@@ -20,7 +20,7 @@ class Admin::GenresController < ApplicationController
   def update
      @genre = Genre.find(params[:id])
      @genre.update(genre_params)
-     redirect_to admin_genres_path
+     redirect_to admin_genres_path(@genre.id)
   end
 
 private
