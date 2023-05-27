f9071cffc0a136b5b62559057fd0c9aa5da07378
728d <sakichichaaaaan@gmail.com>
2023-05-27 04:57:56 +0000

    注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,13 +3,16 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details.all
   end
-
+  
   def update
     @order = Order.find(params[:id])
-    @order.update(order_params)
-    redirect_to admin_order_path(@order.id)
+    @order_details = OrderDetail.where(order_id: params[:id])
+    
+    if @order.update(order_params)
+      @order_details.update_all(crafting_status: 1) if @order.order_status == "payment_confirmed"
+    end
+    redirect_to admin_order_path(@order)
   end
-
 
   private
 
