78f45d937ef11cd5d6c65b97e006be73ae197676
keynoup <126676009+keynoup@users.noreply.github.com>
2023-05-27 05:28:44 +0000

    新着順

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < ApplicationController
-  def show
-     @orders = Order.where(customer_id: params[:id]).page(params[:page])
-     @order = Order.find_by(customer_id:params[:id])
+  def order_customer
+     @orders = Order.where(customer_id: params[:id]).page(params[:page]).order(created_at: :desc)
+     @order = Order.find_by(customer_id: params[:id])
   end
 end
